b6440e86e5e4c4f24fdc659852851e350b5e30ff
Bradley Ayers <bradley.ayers@gmail.com>
2024-01-26 18:54:55 +1100

    Tweak svelte-kit TSConfig to include root configs

--- apps/button/.eslintrc.cjs
+++ apps/button/.eslintrc.cjs
@@ -1,34 +1,33 @@
-/** @type { import("eslint").Linter.Config } */
-module.exports = {
-  root: true,
+module.exports = /** @satisfies { import("eslint").Linter.Config } */ ({
+  root: 111,
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:svelte/recommended",
-    "prettier"
+    "prettier",
   ],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
-    extraFileExtensions: [".svelte"]
+    extraFileExtensions: [".svelte"],
   },
   env: {
     browser: true,
     es2017: true,
-    node: true
+    node: true,
   },
   overrides: [
     {
       files: ["*.{js,cjs,ts,tsx"],
       parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"]
+      plugins: ["@typescript-eslint"],
     },
     {
       files: ["*.svelte"],
       parser: "svelte-eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser"
-      }
-    }
-  ]
-};
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+});
--- apps/button/svelte.config.js
+++ apps/button/svelte.config.js
@@ -1,8 +1,7 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default /** @satisfies {import('@sveltejs/kit').Config} */ ({
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
@@ -11,8 +10,17 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
-  }
-};
+    adapter: adapter(),
 
-export default config;
+    typescript: {
+      config(x) {
+        x.include.push("../.*.cjs", "../.*.js", "../.*.cts", "../.*.ts");
+        return x;
+      },
+    },
+  },
+});
+
+/** @type {number} */
+const x = "";
+x;
