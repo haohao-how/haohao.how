a601b3c49212e1ca15a95d9a9713fbac6e91db64
Bradley Ayers <bradley.ayers@gmail.com>
2024-01-28 14:07:32 +1100

    Add Sentry monitoring

---
+++ projects/frontend/src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from "@sentry/sveltekit";
+
+Sentry.init({
+  dsn: "https://88e3787d84756d748f01113cc6a01fde@o4506645802909696.ingest.sentry.io/4506645804679168",
+  tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
