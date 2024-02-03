0bce265fb8501066fd89c5a6ac0a95389770753d
Bradley Ayers <bradley.ayers@gmail.com>
2024-02-03 12:38:36 +1100

    Add `ipAddress` column.

---
+++ projects/frontend/drizzle/0004_mighty_nextwave.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "clickCount" ADD COLUMN "ipAddress" "inet";
--- projects/frontend/src/schema.ts
+++ projects/frontend/src/schema.ts
@@ -1,9 +1,10 @@
-import { bigint, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { bigint, inet, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const clickCount = pgTable("clickCount", {
   id: uuid("id").primaryKey().defaultRandom(),
   sessionId: uuid("sessionId"),
   clickCount: bigint("clickCount", { mode: "number" }).notNull().default(0),
+  ipAddress: inet("ipAddress"),
   createdAt: timestamp("createdAt").defaultNow().notNull(),
   updatedAt: timestamp("updatedAt").defaultNow().notNull(),
 });
