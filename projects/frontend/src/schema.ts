6deaec1ea20e49292ce2fff5cd378199a3409721
Matt Davidson <matt@mattdavidson.kiwi>
2024-02-04 11:44:35 +1100

    update schema.ts

--- projects/frontend/src/schema.ts
+++ projects/frontend/src/schema.ts
@@ -1,9 +1,17 @@
-import { bigint, inet, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { bigint, inet, pgSchema, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const clickCount = pgTable("clickCount", {
+export const schema = pgSchema("haohaoHow");
+
+export const clickCount = schema.table("clickCount", {
   id: uuid("id").primaryKey().defaultRandom(),
-  sessionId: uuid("sessionId"),
+  sessionId: uuid("sessionId").references(() => session.id),
   clickCount: bigint("clickCount", { mode: "number" }).notNull().default(0),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+});
+
+export const session = schema.table("session", {
+  id: uuid("id").primaryKey().defaultRandom(),
   ipAddress: inet("ipAddress"),
   createdAt: timestamp("createdAt").defaultNow().notNull(),
   updatedAt: timestamp("updatedAt").defaultNow().notNull(),
