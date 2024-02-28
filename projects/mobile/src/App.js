353b4dc55abe0f3dcaf1cb6a463e2c4c06a68065
Bradley Ayers <bradley.ayers@gmail.com>
2024-02-28 14:24:43 +1100

    Customize expo App.js

--- projects/mobile/src/App.js
+++ projects/mobile/src/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "green" }}>Hello world!</Text>
       <StatusBar style="auto" />
+      <Text>Matt</Text>
     </View>
   );
 }
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "pink",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
