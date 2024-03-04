1a098a058031db7a6f807d38eb64f15363aba8aa
Bradley Ayers <bradley.ayers@gmail.com>
2024-03-05 00:50:43 +1100

    Push an EAS update

--- projects/frontend/src/App.js
+++ projects/frontend/src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <Text style={{ fontWeight: "bold" }}>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
