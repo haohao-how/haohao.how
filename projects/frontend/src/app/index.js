7ed8e65d062ff5defc42486db98f36e86f196c52
Bradley Ayers <bradley.ayers@gmail.com>
2024-03-08 12:21:57 +1100

    Add a pressable button

--- projects/frontend/src/app/index.js
+++ projects/frontend/src/app/index.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Pressable, Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function IndexPage() {
+  const [timesPressed, setTimesPressed] = useState(0);
+
+  let textLog = "";
+  if (timesPressed > 1) {
+    textLog = timesPressed + "x onPress";
+  } else if (timesPressed > 0) {
+    textLog = "onPress";
+  }
+
   return (
     <View style={styles.container}>
       <Text style={{ fontWeight: "bold", color: "white" }}>Hello, world!</Text>
       <Text style={{ fontWeight: "bold", color: "white" }}>👋 🤜🤛</Text>
-      <Link href="/learn">Go to /learn</Link>
+      <Link href="/learn">
+        <Pressable
+          onPress={() => {
+            setTimesPressed((current) => current + 1);
+          }}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+            },
+            styles.wrapperCustom,
+          ]}
+        >
+          {({ pressed }) => (
+            <Text style={styles.text} selectable={false}>
+              {pressed ? "Release!" : "Go to /learn"}
+            </Text>
+          )}
+        </Pressable>
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +47,12 @@ const styles = StyleSheet.create({
     backgroundColor: "tomato",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 16,
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6,
   },
 });
