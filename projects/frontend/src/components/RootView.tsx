09ac13def076ec4cfc5860409e7fd461db91bea2
Bradley Ayers <bradley.ayers@gmail.com>
2024-03-09 10:45:39 +1100

    Fix top background color on web
    Introduces `RootView` that on web sets the body background color. It seems like
    there should be a more "built-in" way to do this but perhaps not…

---
+++ projects/frontend/src/components/RootView.tsx
@@ -0,0 +1,28 @@
+import { Platform, View } from "react-native";
+import { PropsOf } from "./types";
+
+/**
+ * Ensures on web that the top status bar background color matches the view background.
+ */
+export function RootView({
+  backgroundColor,
+  ...viewProps
+}: { backgroundColor: string } & PropsOf<typeof View>) {
+  return (
+    <>
+      <View {...viewProps} style={[viewProps.style, { backgroundColor }]} />
+      {
+        // On iOS Safari the top status bar area of the screen color is white by default.
+        Platform.OS === "web" ? (
+          // Inspired by how expo sets the reset styles
+          // https://github.com/expo/expo/blob/44c3a60b1be80a475f59782c64c3b4909c88d7d3/packages/expo-router/src/static/html.tsx#L12
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `body{background-color: ${backgroundColor}`,
+            }}
+          />
+        ) : null
+      }
+    </>
+  );
+}
