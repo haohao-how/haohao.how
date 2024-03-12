98935fc8d1c3911aeaa5580906d3694ae50c8bfb
Bradley Ayers <bradley.ayers@gmail.com>
2024-03-12 14:47:15 +1100

    Make the quiz interactive

--- projects/frontend/src/components/FourUpQuiz.tsx
+++ projects/frontend/src/components/FourUpQuiz.tsx
@@ -1,132 +1,156 @@
 import { Image } from "expo-image";
-import { Link } from "expo-router";
+import { chunk } from "lodash-es";
+import { forwardRef, useCallback, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { RectButton } from "./RectButton";
+import { PropsOf } from "./types";
 
 const buttonThickness = 4;
+const gap = 16;
 
-export const FourUpQuiz = () => {
-  const gap = 16;
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, gap: gap + buttonThickness }}>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 24 }}>
-          <Image
-            source={require("../../assets/cog.svg")}
-            style={{ flexShrink: 1, width: 33, height: 33 }}
-          />
-          <View
-            style={{
-              backgroundColor: "#3A464E",
-              height: 16,
-              flex: 1,
-              borderRadius: 8,
-            }}
-          >
+export enum FourUpQuizFlag {
+  WeakWord,
+}
+
+export const FourUpQuiz = Object.assign(
+  ({
+    prompt,
+    answer,
+    flag,
+    choices,
+    onNext,
+  }: {
+    prompt: string;
+    answer: string;
+    flag?: FourUpQuizFlag;
+    choices: readonly string[];
+    onNext: (success: boolean) => void;
+  }) => {
+    const [selectedChoice, setSelectedChoice] = useState<string>();
+
+    const choicesRows = chunk(choices, 2);
+
+    const handleSubmit = () => {
+      const success = selectedChoice === answer;
+      console.log("success", success);
+      onNext(success);
+    };
+
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, gap: gap + buttonThickness }}>
+          {flag === FourUpQuizFlag.WeakWord ? (
             <View
-              style={{
-                backgroundColor: "#3F4CF5",
-                height: 16,
-                width: "80%",
-                flex: 1,
-                borderRadius: 8,
-              }}
+              style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
             >
-              <View
+              <Image
+                source={require("../../assets/target-red.svg")}
+                style={{ flexShrink: 1, width: 33, height: 30 }}
+              />
+              <Text
                 style={{
-                  backgroundColor: "#6570F6",
-                  height: 5,
-                  marginLeft: 8,
-                  marginRight: 8,
-                  marginTop: 4,
-                  borderRadius: 2,
+                  flex: 1,
+                  color: "#EC5A53",
+                  fontSize: 16,
+                  fontWeight: "bold",
+                  textTransform: "uppercase",
+                  textShadowColor: "black",
+                  textShadowRadius: 1,
                 }}
-              ></View>
+              >
+                Weak word
+              </Text>
             </View>
-          </View>
-        </View>
+          ) : null}
 
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-          <Image
-            source={require("../../assets/target-red.svg")}
-            style={{ flexShrink: 1, width: 33, height: 30 }}
-          />
-          <Text
-            style={{
-              flex: 1,
-              color: "#EC5A53",
-              fontSize: 16,
-              fontWeight: "bold",
-              textTransform: "uppercase",
-              textShadowColor: "black",
-              textShadowRadius: 1,
-            }}
-          >
-            Weak word
-          </Text>
-        </View>
-        <View>
-          <Text
-            style={{
-              color: "white",
-              fontSize: 24,
-              fontWeight: "bold",
-            }}
-          >
-            Select the correct word for the character “dǔ”
-          </Text>
-        </View>
-        {/* Top row */}
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            gap,
-          }}
-        >
-          <AnswerButton text="好" />
-          <AnswerButton text="爱" />
-        </View>
-        {/* Bottom row */}
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            gap,
-          }}
-        >
-          <AnswerButton text="别" />
-          <AnswerButton text="姆" />
-        </View>
-        <Link href="/" asChild>
-          <RectButton color="#A1D151">
+          <View>
             <Text
-              selectable={false}
               style={{
-                textTransform: "uppercase",
-                color: "#161F23",
-                fontSize: 16,
+                color: "white",
+                fontSize: 24,
                 fontWeight: "bold",
-                paddingBottom: 4,
-                paddingTop: 4,
               }}
             >
-              Check
+              {prompt}
             </Text>
-          </RectButton>
-        </Link>
+          </View>
+          {choicesRows.map((choicesRow, i) => (
+            <View style={styles.answerRow} key={i}>
+              {choicesRow.map((choice, i) => (
+                <AnswerButton
+                  text={choice}
+                  selected={choice === selectedChoice}
+                  onPress={setSelectedChoice}
+                  key={i}
+                />
+              ))}
+            </View>
+          ))}
+          <SubmitButton
+            disabled={selectedChoice == null}
+            onPress={handleSubmit}
+          />
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  },
+  { Flag: FourUpQuizFlag },
+);
 
-const AnswerButton = ({ text }: { text: string }) => {
+const SubmitButton = forwardRef<
+  View,
+  { disabled: boolean } & Pick<PropsOf<typeof RectButton>, "onPress">
+>(({ disabled, ...rectButtonProps }, ref) => {
+  const color = disabled ? "#3A464E" : "#A1D151";
+  const textColor = disabled ? "#56646C" : "#161F23";
+
+  return (
+    <RectButton
+      color={color}
+      thickness={disabled ? 0 : undefined}
+      ref={ref}
+      {...(disabled ? null : rectButtonProps)}
+    >
+      <Text
+        selectable={false}
+        style={{
+          textTransform: "uppercase",
+          color: textColor,
+          fontSize: 16,
+          fontWeight: "bold",
+          paddingBottom: 4,
+          paddingTop: 4,
+        }}
+      >
+        Check
+      </Text>
+    </RectButton>
+  );
+});
+
+const AnswerButton = ({
+  selected,
+  text,
+  onPress,
+}: {
+  selected: boolean;
+  text: string;
+  onPress: (text: string) => void;
+}) => {
+  const handlePress = useCallback(() => {
+    onPress(text);
+  }, [text]);
+
+  const color = selected ? "#232E35" : "#161F23";
+  const accentColor = selected ? "#5183A4" : "#3A464E";
+
   return (
     <RectButton
       borderWidth={2}
       thickness={buttonThickness}
-      color="#161F23"
-      accentColor="#3A464E"
+      color={color}
+      accentColor={accentColor}
+      onPress={handlePress}
       style={{ flex: 1 }}
     >
       <Text style={{ color: "white", fontSize: 80 }} selectable={false}>
@@ -142,5 +166,10 @@ const styles = StyleSheet.create({
     gap: 8,
     alignItems: "center",
     justifyContent: "center",
+  },
+  answerRow: {
+    flex: 1,
+    flexDirection: "row",
+    gap,
   },
 });
