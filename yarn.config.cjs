bb05c90a9531be8f8e69e4a068e432bf1c7948e0
Bradley Ayers <bradley.ayers@gmail.com>
2024-05-04 17:58:29 +0800

    Some dependency improvements
    - Add Yarn constraints
    - Align versions of dependencies across workspaces
    - Add root typechecking
    - Upgrade Vercel

---
+++ yarn.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('@yarnpkg/types')} */
+const { defineConfig } = require("@yarnpkg/types");
+
+/**
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Workspace} Workspace
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Dependency} Dependency
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Context} Context
+ */
+
+/**
+ * This rule will enforce that a workspace MUST depend on the same version of
+ * a dependency as the one used by the other workspaces.
+ *
+ * @param {Context} context
+ */
+function enforceConsistentDependenciesAcrossTheProject({ Yarn }) {
+  for (const dependency of Yarn.dependencies()) {
+    if (dependency.type === `peerDependencies`) continue;
+
+    for (const otherDependency of Yarn.dependencies({
+      ident: dependency.ident,
+    })) {
+      if (otherDependency.type === `peerDependencies`) continue;
+
+      dependency.update(otherDependency.range);
+    }
+  }
+}
+
+module.exports = defineConfig({
+  async constraints({ Yarn }) {
+    enforceConsistentDependenciesAcrossTheProject({ Yarn });
+  },
+});
