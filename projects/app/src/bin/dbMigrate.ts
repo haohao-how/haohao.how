44fcf4670d404782e4dc7bba694b1bd2931399d3
Bradley Ayers <bradley.ayers@gmail.com>
2024-07-05 07:51:09 +0800

    Use Expo API routes

--- projects/app/src/bin/dbMigrate.ts
+++ projects/app/src/bin/dbMigrate.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { createPool } from "../api/db.js";
-import * as schema from "../api/schema.js";
+import { createPool } from "../server/db.js";
+import * as schema from "../server/schema.js";
 
 const pool = createPool();
 const client = await pool.connect();
