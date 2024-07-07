8f691e0dcdb789c29e81ee28db30380add96b22a
Bradley Ayers <bradley.ayers@gmail.com>
2024-07-07 21:22:04 +0800

    Rearrange some stuff

--- projects/app/bin/dbMigrate.ts
+++ projects/app/bin/dbMigrate.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { createPool } from "../server/db.js";
-import * as schema from "../server/schema.js";
+import { createPool } from "../src/server/db.js";
+import * as schema from "../src/server/schema.js";
 
-const pool = createPool();
+const pool = await createPool();
 const client = await pool.connect();
 
 await migrate(drizzle(client, { schema }), {
